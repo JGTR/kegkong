0959a216f9e4613391d0e7b211a529ef4f0bf12b
Jordan Trevino <jordan.trevino@gmail.com>
2013-07-07 17:37:08 -0400

    check_volume method for keg

--- lib/keg.rb
+++ lib/keg.rb
@@ -2,20 +2,19 @@ require "sinatra/activerecord"
 
 class Keg < ActiveRecord::Base
   
-  has_many :measurements
+  has_many :measurements, :dependent => :destroy
 
-  def self.get_data(sp)
-    while true do 
-      sp_char = sp.getc
-
-      if sp_char && sp_char.start_with?("pulses:")
-        number = sp_char.split(":")[1]
-        puts number
+  def check_volume
+    volume = self.max_volume.to_f
+    if self.measurements.size > 0
+      self.measurements.each do |m|
+        puts volume
+        volume = volume - m.change_in_volume.to_f
+        # puts m.change_in_volume.to_f
       end
     end
+    volume
   end
-
-
 
 end
 
