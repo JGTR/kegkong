e59d005c0eed0dc3a7deb62bac7ea66a49eba8d3
Jordan Trevino <jordan.trevino@gmail.com>
2013-07-09 15:44:53 -0400

    fixed sql

--- serial_test.rb
+++ serial_test.rb
@@ -2,13 +2,12 @@
 #using the SerialPort gem
 #(http://rubygems.org/gems/serialport)
 
-I
 require 'rubygems'
 require "serialport"
 require 'sqlite3'
-db = SQLite3::Database.new "database.db"
-# set :database, "sqlite3:///database.db"
+require 'debugger'
 
+db = SQLite3::Database.new "database.db"
 
 #params for serial port
 port_str = "/dev/ttyACM0"  #may be different for you
@@ -19,15 +18,11 @@ parity = SerialPort::NONE
 
 sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
 
-data = []
-counter = 0
-
-while true do 
+while true do
+ keg_id =  db.execute("SELECT id FROM kegs ORDER BY ID DESC LIMIT 1")[0][0]
  pulses = sp.gets("\r\n")
- keg_id = db.execute("SELECT id FROM kegs ORDER BY ID DESC LIMIT 1;")
- now = Time.now
  db.execute("INSERT INTO measurements VALUES(null, :pulses, 2.0, :keg_id, null, null)", {:pulses => pulses, :keg_id => keg_id}) 
 end
 
 
-sp.close   
+# sp.close   
